3606b9d550a7eda4d5e7293f03f3a6978d4de2fe
maxpnz <maxpnzofficial@gmail.com>
2016-03-30 12:24:49 +0400

    initial commit

---
+++ TRPO/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace TRPO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить элемент в массив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(16, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 55);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Удалить одинаковые элементы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(253, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(235, 55);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Очистить массив";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(235, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Добавить массив из файла";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox2.Location = new System.Drawing.Point(16, 102);
+            this.textBox2.MaxLength = 5;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 26);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Поле ввода элемента";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Массив";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(16, 236);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(235, 32);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Сохранить в файл";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(253, 236);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(235, 32);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Выход";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(571, 280);
+            this.ControlBox = false;
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Качество ТРПО -  Лабораторная работа";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ TRPO/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace TRPO
+{
+    public partial class Form1 : Form
+    {
+        int[] Mass = new int[100];
+        int i;
+        FileInfo file = new FileInfo("array.txt");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static bool StringIsValid(string str)   //Проверка файла на буквы и пустоту
+        {
+            return !string.IsNullOrEmpty(str) && Regex.IsMatch(str, @"^[1-9 ]+$");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Добавление элемента в массив
+            if ((textBox2.Text != null) & (textBox2.Text != ""))
+            {
+                if ((Convert.ToInt32(textBox2.Text) < 30000) & (Convert.ToInt32(textBox2.Text) > 0) & (i < 100))
+                {
+                    Mass[i] = Convert.ToInt32(textBox2.Text);
+                    i++;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = false;
+                }
+                else
+                    MessageBox.Show("Введено недопустимое число или превышено максимальное число элементов!!!");
+               
+            }
+            else
+            {
+                MessageBox.Show("Поле ввода пустое");
+            }
+            Output();
+            textBox2.Text = null;
+
+        }
+        public void Output()
+        {
+            //Вывод массив
+            label3.Text = null;
+            for (int k = 0; k < i; k++)
+            {
+                label3.Text = label3.Text + Convert.ToString(Mass[k]) + " ";
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //ограничение на ввод
+            if ((e.KeyChar <= 47 || e.KeyChar >= 58) && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Array.Clear(Mass, 0, Mass.Length);
+            i = 0;
+            label3.Text = null;
+            button1.Enabled = true;
+            button3.Enabled = true;
+            button2.Enabled = false;
+            button4.Enabled = true;
+
+        }
+
+        //Удаление одинаковых элементов
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] result = Mass.Reverse().Distinct().Reverse().ToArray();
+            label3.Text = null;
+            int index = result.Length;
+            if (result[index-1] != 0)
+            {
+                for (int i = 0; i < result.Length; i++)         //Вывод на в текстбокск результат
+                {
+                    label3.Text = label3.Text + result[i].ToString() + " ";
+                };
+            }
+               else          //В случае если в конец пишется ноль, не выводим
+            {
+                for (int i = 0; i < result.Length-1; i++)         //Вывод на в текстбокск результат
+                {
+                    label3.Text = label3.Text + result[i].ToString() + " ";
+                };
+            }
+            Array.Clear(Mass, 0, Mass.Length);
+            Array.Clear(result,0,result.Length);
+            i = 0;
+            button2.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            label3.Text = null;
+            bool over = false;
+            StreamReader streamReader = new StreamReader("array.txt");
+            string filedata = "";
+            if (file.Exists == false)
+            {
+                MessageBox.Show("Файл не был создан");
+            }
+            else
+            {
+                while (!streamReader.EndOfStream)   //Взяли строку из файла
+                {
+                    filedata += streamReader.ReadLine();
+                }
+                if (StringIsValid(filedata))   //Если в строке нет букв и она не пуста
+                {
+                    string[] checker = filedata.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);  //Делим на элементы
+                    int lenght = checker.GetLength(0);
+                    for (i = 0; i < checker.Length; i++)
+                    {
+                        if (checker[i].Length > 5)
+                            over = true;
+                    }
+                    if (lenght < 100)
+                    {
+                        if (!over)
+                        {
+                            Mass = checker.Select(int.Parse).ToArray();  // Успешно. Преобразовываем в int и забиваем массив
+                            for (int i = 0; i < Mass.Length; i++)         //Вывод на в текстбокск результат
+                                {
+                                    label3.Text = label3.Text + Mass[i].ToString() + " ";
+                                }
+                            label3.Text = label3.Text + Environment.NewLine;
+                            i = lenght;
+                            MessageBox.Show("Массив из файла успешно загружен!!!");
+                            button1.Enabled = false;
+                            button2.Enabled = true;
+                            button3.Enabled = true;
+                            button4.Enabled = false;
+                        }
+                        else
+                        {
+                            MessageBox.Show("В массиве введены большие числа");
+                        }
+                            }
+                    else
+                    {
+                        MessageBox.Show("Кол-во элементов в массиве больше 100!!!");
+                    };
+                }
+                else
+                {
+                    MessageBox.Show("Неверный формат входного файла!!! Удалите буквы из файла или заполните его положительными числами через пробел");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
